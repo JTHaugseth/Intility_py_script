61afd1762c615038cc41c5ee4c7d73e6f2b53935
JTHaugseth <97683128+JTHaugseth@users.noreply.github.com>
2023-09-15 02:12:47 +0200

    Update clickScript.py

--- clickScript.py
+++ clickScript.py
@@ -10,20 +10,20 @@ while True:
     first_place_name = scoreboard[0]['name']
     first_place_score = scoreboard[0]['score']
 
-    if first_place_name != "Julian Haugseth":
+    
 
-        your_new_score = first_place_score + 1
+    your_new_score = first_place_score + 1
 
-        print("New score found, posting new highscore")
+    print("New score found, posting new highscore")
         
-        payload = {
+    payload = {
             'name': 'Julian Haugseth',  
             'number': '40087795',  
-            'score': your_new_score,
+            'score': 179769313486231570814527423731704356798070567525844996598917476803157260780028538760589558632766878171540458953514382464234321326889464182768467546703537516986049910576551282076245490090389328944075868508455133942304583236903222948165808559332123348274797826204144723168738177180919299881250404026184124858369,
             'school': 'HØyskolen Kristiania'  
         }
 
-        response = requests.post(
+    response = requests.post(
             "https://intility-games.intility.com/?index=&_data=routes%2Findex",
             data=payload,
             headers={
@@ -31,6 +31,6 @@ while True:
             }
         )
 
-        print(response.status_code)
+    print(response.status_code)
     
-    time.sleep(120)
+    time.sleep(500)
